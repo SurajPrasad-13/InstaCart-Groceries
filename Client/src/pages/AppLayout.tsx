65535ac264f6a9f669ef83c2c3c8f35983daf092
Suraj Prasad-13 <spyadav0413@gmail.com>
2026-06-09 01:19:49 +0530

    created navbar and Banner

---
+++ Client/src/pages/AppLayout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom"
+import Banner from "../components/Banner"
+import Navbar from "../components/Navbar"
+
+const AppLayout = () => {
+  return (
+    <>
+  <Banner/>
+    <Navbar/>
+    <main className="min-h-screen">
+      <Outlet/>
+    </main>
+    <p>footer</p>
+    <p>cart sidebar</p>
+    </>
+  )
+}
+
+export default AppLayout
